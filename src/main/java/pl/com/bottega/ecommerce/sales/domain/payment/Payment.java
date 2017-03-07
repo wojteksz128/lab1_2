6bc2877e9c65f1b48da6307d152b040e6347ac20
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-07 20:40:27 +0100

    Podpunkt 4.1 - utworzenie PaymentFactory

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
@@ -22,11 +22,9 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class Payment {
 
 	private ClientData clientData;
-
 	private Money amount;
-
 	private Id aggregateId;
-
+	private PaymentFactory pFactory;
 
 	public Payment(Id aggregateId, ClientData clientData, Money amount) {
 		this.aggregateId = aggregateId;
@@ -35,8 +33,7 @@ public class Payment {
 	}
 
 	public Payment rollBack() {
-		Id id = Id.generate();
-
-		return new Payment(id, clientData, amount.multiplyBy(-1));		
+		
+		return pFactory.create(clientData, amount.multiplyBy(-1));		
 	}
 }
